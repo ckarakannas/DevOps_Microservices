624f97d89582cae24552303ad50fb1dee63a3a16
Christodoulos Karakannas <christodoulos.karakannas@gmail.com>
2021-06-24 16:08:46 +0100

    Added Docker support

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
+# Run Docker locally
 
-# Step 1:
 # Build image and add a descriptive tag
+docker build -t chrisk14/udacity-ml-api .
 
-# Step 2: 
 # List docker images
+docker image ls
 
-# Step 3: 
 # Run flask app
+docker run -p 8000:5000 chrisk14/udacity-ml-api
